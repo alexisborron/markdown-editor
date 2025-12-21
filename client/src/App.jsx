254d828de2bf48f06568adcb37b309184a82754a
alexisborron <188102818+alexisborron@users.noreply.github.com>
2025-12-21 14:46:23 -0600

    feat: add basic markdown editor with live preview

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
 const App = () => {
-  return <></>;
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <h1>Markdown</h1>
+        <textarea value={text} onChange={handleChange} />
+        <section>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default App;
